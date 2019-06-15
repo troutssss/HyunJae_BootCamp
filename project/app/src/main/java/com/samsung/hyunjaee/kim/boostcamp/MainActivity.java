795a3891f089db8268a55ec1d7ecabb115f3fdf9
김현재 <hyunjae@gimhyeonjaeui-MacBookPro.local>
2019-06-16 03:39:07 +0900

    first android project setting

---
+++ project/app/src/main/java/com/samsung/hyunjaee/kim/boostcamp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.samsung.hyunjaee.kim.boostcamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
